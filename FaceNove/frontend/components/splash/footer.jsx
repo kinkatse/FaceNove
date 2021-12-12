80223e231e7977c20343268149b216e535b374ff
kakintse <kinkatse@mail.adelphi.edu>
2021-12-12 14:46:22 -0500

    Include CSS Stylesheets and also gave components more substance

--- FaceNove/frontend/components/splash/footer.jsx
+++ FaceNove/frontend/components/splash/footer.jsx
@@ -12,6 +12,11 @@ class Footer extends React.Component {
         return (
             <div className="footer">
                 <h1>footer</h1>
+                <p className="footer-message">Kin Ka Tse</p>
+                <p className="footer-message">Clone of Facebook</p>
+                <a href="https://github.com/kinkatse" className="footer-link">Github</a>
+                <a href="https://www.linkedin.com/in/kin-ka-tse/" className="footer-link">LinkedIn</a>
+                <a href="https://github.com/kinkatse/FaceNove" className="footer-link">Repo Link</a>
             </div>
         )
     }
