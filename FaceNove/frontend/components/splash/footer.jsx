a1ff9f62700bc525b35bcf0470d2199229a1ac76
kakintse <kinkatse@mail.adelphi.edu>
2021-12-12 17:57:04 -0500

    Add styling to footer for splash

--- FaceNove/frontend/components/splash/footer.jsx
+++ FaceNove/frontend/components/splash/footer.jsx
@@ -11,14 +11,18 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
-                <h1>footer</h1>
-                <p className="footer-message">Kin Ka Tse</p>
-                <p className="footer-message">Clone of Facebook</p>
-                <a href="https://github.com/kinkatse" className="footer-link">Github</a>
-                <a href="https://www.linkedin.com/in/kin-ka-tse/" className="footer-link">LinkedIn</a>
-                <a href="https://angel.co/u/kin-ka-tse" className="footer-link">AngelList</a>
-                <a href="https://github.com/kinkatse/FaceNove" className="footer-link">Repo Link</a>
-                <a href="" className="footer-link">Portfolio</a>
+                <div className="footer-info">
+                    <span>footer</span>
+                    <span className="footer-message">Kin Ka Tse</span>
+                    <span className="footer-message">Clone of Facebook</span>
+                </div>
+                <div className="footer-links">
+                    <a href="https://github.com/kinkatse" className="footer-link">Github</a>
+                    <a href="https://www.linkedin.com/in/kin-ka-tse/" className="footer-link">LinkedIn</a>
+                    <a href="https://angel.co/u/kin-ka-tse" className="footer-link">AngelList</a>
+                    <a href="https://github.com/kinkatse/FaceNove" className="footer-link">Repo Link</a>
+                    <a href="" className="footer-link">Portfolio</a>
+                </div>
             </div>
         )
     }
