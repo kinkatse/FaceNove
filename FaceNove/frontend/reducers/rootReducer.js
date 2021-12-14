7bc0823400e179d7bc650211c418af4090924516
kakintse <kinkatse@mail.adelphi.edu>
2021-12-13 22:59:42 -0500

    Make Root Reducer and actions/reducers folder inside frontend

---
+++ FaceNove/frontend/reducers/rootReducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+// This allows us to combine many reducers into one
+// for the createStore when we initialize it since
+// it can only take one reducer as a parameter
+
+// Then we would import from any reducer after
+
+const rootReducer = combineReducers({
+    // Example:
+    // fruitReducer: fruitReducer
+    // Although we can simplify this by just
+    // fruitReducer since it is the same key
+    // as value and (something, React?) knows that
+})
+
+export default rootReducer;
