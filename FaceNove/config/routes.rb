4e13cdd67e9c9d6d645d17001187ab9f5098f82b
kakintse <kinkatse@mail.adelphi.edu>
2021-12-13 23:45:52 -0500

    Make Routes for Users and Sessions

--- FaceNove/config/routes.rb
+++ FaceNove/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
+  # This allows us to make the root of our app path display this view
+
+  # namespace allows us to have a subset of controllers that live under
+  # a specific url
+  namespace :api do
+    resources :users, only: [:create, :index, :show, :update, :destroy]
+    resource :sessions, only: [:create, :destroy]
+  end
 end
