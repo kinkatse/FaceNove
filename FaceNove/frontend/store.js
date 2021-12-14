19bcfcfebdd1e3e021231305699f6f0d210ddd38
kakintse <kinkatse@mail.adelphi.edu>
2021-12-13 23:35:54 -0500

    Add some comments

--- FaceNove/frontend/store.js
+++ FaceNove/frontend/store.js
@@ -2,7 +2,8 @@ import { createStore } from 'redux'
 // Import createStore function from redux which will
 // allow us to initialize the store with a reducer.
 // Later we will add the middleware as another import
-// from the 'redux' library
+// from the 'redux' library. We would also need thunk
+// and logger
 
 // Here we import the rootReducer and pass this in as
 // the first argument for createStore. Make sure we
@@ -13,6 +14,9 @@ import rootReducer from './reducers/rootReducer'
 // which is something we may need from a previous state
 // and use that as the state parameter that createStore takes
 // but if there is no preloadedState, then it defaults to {}
+// For our purposes, the preloadedState will help us determine
+// if the current user is still logged in and so even when we
+// refresh the page, we should still keep the state
 const configureStore = (preloadedState = {}) => (
     createStore(rootReducer, preloadedState)
 )
