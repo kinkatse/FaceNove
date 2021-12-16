06c6af81dad26daec06cba519395cd84aee1db22
kakintse <kinkatse@mail.adelphi.edu>
2021-12-15 21:38:21 -0500

    Create a better application state pt 1

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,8 +2,7 @@ import { combineReducers } from 'redux';
 // This allows us to combine many reducers into one for the createStore
 // when we initialize it since it can only take one reducer as a parameter
 
-// Then we would import from any reducer after
-import colorReducer from './color_reducer';
+// Import reducers for each slice of state
 
 const rootReducer = combineReducers({
     // Example:
@@ -12,7 +11,10 @@ const rootReducer = combineReducers({
     // that is just syntactic sugar. Need our state to have multiple slices of state so we need
     // many different reducers that handle different parts of the app like sessions, ui, entities,
     // errors etc, so we shouldn't just do one reducer per functionality, that may become very tedious
-    color: colorReducer
+    entities,
+    session,
+    errors,
+    ui
 })
 
 export default rootReducer;
