dc821866acaef6362d5725879a7b878f52ca33f7
kakintse <kinkatse@mail.adelphi.edu>
2021-12-17 01:43:54 -0500

    Finish ApplicationController methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,18 +8,40 @@ class ApplicationController < ActionController::Base
     private
 
     def current_user
+        # If no session token return nil, remember session hash is provided by rails
+        return nil if !session[:session_token]
+        # If there exists a session token, we key into it and get the value of the session token and make a new key value pair for it and find by that User
+        @currentUser ||= User.find_by(session_token: session[:session_token])
     end
 
     def logged_in?
+        # truth-false value for if current_user exists
+        !!current_user
     end
 
     def log_in(email)
+        # Upon successful log in with email, we reset session token so each time a user logs in, its a new session token
+        # Why bang (!)?
+        email.reset_session_token!
+        # Setting the session token of the email to be the value to the key :session_token of the session hash
+        session[:session_token] = email.session_token
+        @currentUser = email
     end
 
     def log_out
+        # Reset so that even after log out, the session token will be different - I think
+        current_user.reset_session_token!
+        # Setting the session to nil
+        session[:session_token] = nil
+        @currentUser = nil
     end
 
     def require_logged_in
+        # If no logged in user, give an error
+        # What is base?
+        if !current_user
+            render json: { base: ['Invalid credentials'] }, status: 401
+        end
     end
-    
+
 end
