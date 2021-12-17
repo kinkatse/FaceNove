53bafb2b0c9bfb7c7ae9f9065ba0f3f61d6ef82c
kakintse <kinkatse@mail.adelphi.edu>
2021-12-17 00:37:33 -0500

    Write validations for columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :email, :session_token, :password_digest, :firstName, :lastName, :birthdate, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
 end
