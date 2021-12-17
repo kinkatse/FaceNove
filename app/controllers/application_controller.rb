a1ce94e5ab066ef2ef55c5dac7b807f1ff13b3d8
kakintse <kinkatse@mail.adelphi.edu>
2021-12-17 01:36:30 -0500

    Start ApplicationController methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    # Prevents Cross Site Request Forgery Attacks
+    protect_from_forgery with: :exception
+    
+    # This allows access in other classes that inherit this
+    helper_method :current_user, :logged_in?
+
+    private
+
+    def current_user
+    end
+
+    def logged_in?
+    end
+
+    def log_in(email)
+    end
+
+    def log_out
+    end
+
+    def require_logged_in
+    end
+    
 end
