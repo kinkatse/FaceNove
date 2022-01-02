b66afbe812923dd9bf5b0073701a769144489f33
kakintse <kinkatse@mail.adelphi.edu>
2022-01-02 17:10:58 -0500

    No need for login or signup authroutes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 // BIG HEAD ACHE ISSUE: Make sure you import Switch IF we are on react-router-dom version 5.2.0
 // Otherwise, it was changed to be Routes in a later version which gives invalid import/export issue for app and causes confusion
 
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+    // We dont need:
+    // <AuthRoute path="/login" component={}/>
+    // <AuthRoute path="/signup" component={}/>
+    // Because Facebook doesn't have different routes to these pages, they handle login and signup on the splash
+    // Technically it does have a login page but it's already accessible on the splash so we should only care to
+    // make this routes as a bonus later on
+
 import ModalContainer from './modal/modal_container';
 import SplashContainer from './splash/splash_container';
 
