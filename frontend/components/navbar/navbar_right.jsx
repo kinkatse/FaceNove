e75b2c3a6a216e2c3a3c96f5940de19c205db48e
kakintse <kinkatse@mail.adelphi.edu>
2022-01-03 18:56:26 -0500

    Make left navbar color change as well

--- frontend/components/navbar/navbar_right.jsx
+++ frontend/components/navbar/navbar_right.jsx
@@ -4,10 +4,19 @@ import { Link } from 'react-router-dom';
 // Show about links and log out
 
 const NavBarRight = ({ logout, color }) => {
+    let colorSplash;
+    if (color === 'blue') {
+        colorSplash = 'bluesplash';
+    } else if (color === 'green') {
+        colorSplash = 'greensplash'
+    } else if (color === 'red') {
+        colorSplash = 'redsplash'
+    }
+    
     return (
         <div className="navbar_right">
             <h1>This is right of navbar</h1>
-            <button className="splashbutton" onClick={logout}>Log Out!</button>
+            <button className={'logoutbuttontext splashbutton ' + colorSplash} onClick={logout}>Log Out!</button>
         </div>
     )
 }
