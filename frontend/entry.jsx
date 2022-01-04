31635358e037e2dc4128fe2430f80be538a4d2a1
kakintse <kinkatse@mail.adelphi.edu>
2022-01-03 23:13:33 -0500

    Bootstrapping current user pt 2

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -5,7 +5,19 @@ import Root from './components/root'
 import configureStore from './store/store.js'
 
 document.addEventListener("DOMContentLoaded", () => {
-    let store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+            }
+        store = configureStore(preloadedState)
+        delete window.currentUser
+    } else {
+        store = configureStore();
+    }
     window.store = store;
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store}/>, root)
