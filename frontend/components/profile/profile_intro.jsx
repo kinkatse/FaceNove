c0fc327a70b18f3744a19c6add5bfa6321cacf56
kakintse <kinkatse@mail.adelphi.edu>
2022-01-08 11:40:11 -0500

    Include conditional logic to profile intro info

--- frontend/components/profile/profile_intro.jsx
+++ frontend/components/profile/profile_intro.jsx
@@ -9,22 +9,50 @@ class ProfileIntro extends React.Component {
     }
 
     render() {
+        // Later want to implement logic to allow to show certain info fields
         let user = this.props.user;
-        let birthday = user.birthdate.split('-');
-        let birthdayFormatted = birthday.reverse().join('/');
-        // debugger
+        let bio, birthday, birthdayFormatted, gender, education, hometown, work, relationship, website;
+        
+        // Conditionals to make the info be default if no value
+        if (user.bio) { bio = "Bio: " + user.bio }
+        else { bio = "No Bio" }
+
+        if (user.birthdate) { birthday = user.birthdate.split('-') }
+        else { birthday = "Birthday not registered" }
+        if (user.birthdate !== "Birthday not registered") {
+            birthdayFormatted = "Born on " + birthday.reverse().join('/')
+        } else { "Something unexpected happened" }
+
+        if (user.gender) { gender = "Identify as " + user.gender }
+        else { gender = "Gender not registered" }
+
+        if (user.education) { education = "Studied at " + user.education }
+        else { education = "Education not registered" }
+
+        if (user.hometown) { hometown = "Lives in " + user.hometown }
+        else { hometown = "Hometown not registered" }
+
+        if (user.work) { work = "Works as " + user.work }
+        else { work = "Work not registered" }
+
+        if (user.relationship) { relationship = "Currently " + user.relationship }
+        else { relationship = "Relationship not registered" }
+
+        if (user.website) { website = "Website: " + user.website }
+        else { website = "Website not registered" }
+
         return (
             <div className="profile_intro">
                 <h1>Intro</h1>
-                <p className="profile_bio">Bio: {user.bio}</p>
+                <p className="profile_bio">{bio}</p>
                 <p className="profile_info">Email me at {user.email}</p>
-                <p className="profile_info">Born on {birthdayFormatted}</p>
-                <p className="profile_info">Identify as {user.gender}</p>
-                <p className="profile_info">Studied at {user.education}</p>
-                <p className="profile_info">Lives in {user.hometown}</p>
-                <p className="profile_info">Works as {user.work}</p>
-                <p className="profile_info">Relationship Status: {user.relationship}</p>
-                <p className="profile_info">Website: {user.website}</p>
+                <p className="profile_info">{birthdayFormatted}</p>
+                <p className="profile_info">{gender}</p>
+                <p className="profile_info">{education}</p>
+                <p className="profile_info">{hometown}</p>
+                <p className="profile_info">{work}</p>
+                <p className="profile_info">{relationship}</p>
+                <p className="profile_info">{website}</p>
             </div>
         )
     }
