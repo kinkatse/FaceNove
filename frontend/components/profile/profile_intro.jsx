2e8bc954a581ae0015f4c5a82cd26fd251554fb4
kakintse <kinkatse@mail.adelphi.edu>
2022-01-07 22:37:56 -0500

    Adjustment made to the intro of profile page

--- frontend/components/profile/profile_intro.jsx
+++ frontend/components/profile/profile_intro.jsx
@@ -17,11 +17,13 @@ class ProfileIntro extends React.Component {
             <div className="profile_intro">
                 <h1>Intro</h1>
                 <p className="profile_bio">Bio: {user.bio}</p>
-                <p className="profile_info">Birthday: {birthdayFormatted}</p>
-                <p className="profile_info">Education: {user.education}</p>
-                <p className="profile_info">Hometown: {user.hometown}</p>
-                <p className="profile_info">Work: {user.work}</p>
-                <p className="profile_info">Relationship: {user.relationship}</p>
+                <p className="profile_info">Email me at {user.email}</p>
+                <p className="profile_info">Born on {birthdayFormatted}</p>
+                <p className="profile_info">Identify as {user.gender}</p>
+                <p className="profile_info">Studied at {user.education}</p>
+                <p className="profile_info">Lives in {user.hometown}</p>
+                <p className="profile_info">Works as {user.work}</p>
+                <p className="profile_info">Relationship Status: {user.relationship}</p>
                 <p className="profile_info">Website: {user.website}</p>
             </div>
         )
