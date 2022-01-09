f6e8ca62824e3819dbb37c8b431f414e2295a0cb
kakintse <kinkatse@mail.adelphi.edu>
2022-01-08 21:16:39 -0500

    Start making modals for edit, profile pic and cover pic by making actions and reducers

--- frontend/actions/modal_actions.js
+++ frontend/actions/modal_actions.js
@@ -1,9 +1,30 @@
 export const OPEN_SIGNUP_MODAL = 'OPEN_SIGNUP_MODAL';
+export const OPEN_EDIT_MODAL = 'OPEN_EDIT_MODAL';
+export const OPEN_PROFPIC_MODAL = 'OPEN_PROFPIC_MODAL';
+export const OPEN_COVPIC_MODAL = 'OPEN_COVPIC_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 
 export const openSignupModal = () => {
     return {
         type: OPEN_SIGNUP_MODAL
+    }
+}
+
+export const openEditModal = () => {
+    return {
+        type: OPEN_EDIT_MODAL
+    }
+}
+
+export const openProfPicModal = () => {
+    return {
+        type: OPEN_PROFPIC_MODAL
+    }
+}
+
+export const openCovPicModal = () => {
+    return {
+        type: OPEN_COVPIC_MODAL
     }
 }
 
--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -1,10 +1,22 @@
-import { OPEN_SIGNUP_MODAL, CLOSE_MODAL } from "../actions/modal_actions";
+import {
+    OPEN_SIGNUP_MODAL,
+    OPEN_EDIT_MODAL,
+    OPEN_PROFPIC_MODAL,
+    OPEN_COVPIC_MODAL,
+    CLOSE_MODAL
+} from "../actions/modal_actions";
 
 const modalReducer = (state = {type: ''}, action) => {
     Object.freeze(state);
     switch(action.type) {
         case OPEN_SIGNUP_MODAL:
             return Object.assign({}, state, {type: 'signup'})
+        case OPEN_EDIT_MODAL:
+            return Object.assign({}, state, {type: 'edit'})
+        case OPEN_PROFPIC_MODAL:
+            return Object.assign({}, state, {type: 'profilepic'})
+        case OPEN_COVPIC_MODAL:
+            return Object.assign({}, state, {type: 'coverpic'})
         case CLOSE_MODAL:
             return Object.assign({}, state, {type: ''})
         default:
