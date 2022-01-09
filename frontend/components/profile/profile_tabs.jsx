4078c714e0a69dc71b27bc2beaa3c9855665edd2
kakintse <kinkatse@mail.adelphi.edu>
2022-01-09 15:24:20 -0500

    Made tabs change when selected dynamically

--- frontend/components/profile/profile_tabs.jsx
+++ frontend/components/profile/profile_tabs.jsx
@@ -4,8 +4,12 @@ class ProfileTabs extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            activeTab: 0
         }
+    }
+
+    changeActiveTab(num) {
+        this.setState({activeTab: num})
     }
 
     rendersEdit() {
@@ -21,18 +25,63 @@ class ProfileTabs extends React.Component {
         }
     }
 
+    rendersTabs() {
+        const tabs = [
+            "Posts",
+            "About",
+            "Friends",
+            "Photos",
+            "Hobbies",
+            "Settings"
+        ]
+        let active = this.state.activeTab;
+        let tabline = tabs.map((tab, idx) => {
+            let tabclassname = idx === active ? 'tab_active' : 'tab_inactive';
+            return (
+                <li
+                    key={idx}
+                    className={tabclassname}
+                    onClick={ () => {this.changeActiveTab(idx)} }>
+                        {tab}
+                </li>
+            )
+        })
+        return (
+        <ul className="profile_tabs">{tabline}</ul>
+        )
+    }
+
     render() {
-        // When I get to this, should be conditionally rendered components by clicked on
-        // which a variable like active will decide to render it or not
         return (
             <div className="profile_tabs_whole">
-                <ul className="profile_tabs">
-                    <li className="tab_active">Posts</li>    
-                    <li className="tab_inactive">About</li>
-                    <li className="tab_inactive">Friends</li>
-                    <li className="tab_inactive">Photos</li>
-                    <li className="tab_inactive">Hobbies</li>
-                </ul>
+                {this.rendersTabs()}
+                {/* <ul className="profile_tabs">
+                    <li
+                        className={tabclassname}
+                        onClick={this.changeActiveTab}>
+                            Posts
+                    </li>    
+                    <li
+                        className={tabclassname}
+                        onClick={this.changeActiveTab}>
+                            About
+                    </li>
+                    <li
+                        className={tabclassname}
+                        onClick={this.changeActiveTab}>
+                            Friends
+                    </li>
+                    <li
+                        className={tabclassname}
+                        onClick={this.changeActiveTab}>
+                            Photos
+                    </li>
+                    <li
+                        className={tabclassname}
+                        onClick={this.changeActiveTab}>
+                            Hobbies
+                    </li>
+                </ul> */}
                 {this.rendersEdit()}
             </div>
         )
