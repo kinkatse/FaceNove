bf56626e35684f4176994f7ad131d71178e3d7f5
kakintse <kinkatse@mail.adelphi.edu>
2022-01-09 17:03:59 -0500

    Make click listeners occur only when current user is the same user as the user page and reorganized some logic in the components

--- frontend/components/profile/profile_top.jsx
+++ frontend/components/profile/profile_top.jsx
@@ -8,9 +8,8 @@ class ProfileTop extends React.Component {
 
         }
     }
-
-    render() {
-        // debugger
+    
+    rendersProfilePic() {
         let profpicColor;
         if (this.props.color === "blue") {
             profpicColor = "profpicbluebig"
@@ -19,24 +18,65 @@ class ProfileTop extends React.Component {
         } else if (this.props.color === "red") {
             profpicColor = "profpicredbig"
         }
-        
+
+        let eventlistener;
+        if (this.props.currentUserId === parseInt(this.props.userId)) {
+            eventlistener = this.props.openProfPicModal;
+        } else {
+            eventlistener = null;
+        }
+
+        return (
+            <div className={'profilepic ' + profpicColor}>
+                <img
+                    onClick={eventlistener}
+                    src={this.props.user.profilePicUrl}
+                />
+                {/* <h2 onClick={eventlistener}>Change Prof Pic</h2> */}
+            </div>
+        )
+    }
+
+    rendersCoverPic() {
+        let eventlistener;
+        if (this.props.currentUserId === parseInt(this.props.userId)) {
+            eventlistener = this.props.openCovPicModal;
+        } else {
+            eventlistener = null;
+        }
+
+        return (
+            <>
+                <img
+                    className="coverpic"
+                    onClick={eventlistener}
+                    src={this.props.user.coverPicUrl}
+                />
+                {/* <h2 onClick={eventlistener}>Change Cov Pic</h2> */}
+            </>
+        )
+    }
+
+    render() {
         let first = this.props.user.firstName;
         let last = this.props.user.lastName;
         return (
             <div className="profile_top">
-                <img
+                {this.rendersCoverPic()}
+                {/* <img
                     className="coverpic"
                     onClick={this.props.openCovPicModal}
                     src={this.props.user.coverPicUrl}
                 />
-                <h2 onClick={this.props.openCovPicModal}>Change Cov Pic</h2>
-                <div className={'profilepic ' + profpicColor}>
+                <h2 onClick={this.props.openCovPicModal}>Change Cov Pic</h2> */}
+                {this.rendersProfilePic()}
+                {/* <div className={'profilepic ' + profpicColor}>
                     <img
                         onClick={this.props.openProfPicModal}
                         src={this.props.user.profilePicUrl}
                     />
                     <h2 onClick={this.props.openProfPicModal}>Change Prof Pic</h2>
-                </div>
+                </div> */}
                 <p className="profile_title">{first} {last}</p>
                 <ProfileTabs
                     currentUserId={this.props.currentUserId}
