65a631a9396c164e1bc5ad61987122c3ad459266
kakintse <kinkatse@mail.adelphi.edu>
2022-01-09 16:51:54 -0500

    Copy over some simple styling from other modals

--- frontend/components/modal/modal_covpic.jsx
+++ frontend/components/modal/modal_covpic.jsx
@@ -8,8 +8,31 @@ class ModalCovPic extends React.Component {
     }
 
     render() {
+        let colorSplash;
+        if (this.props.color === 'blue') {
+            colorSplash = 'bluesplash';
+        } else if (this.props.color === 'green') {
+            colorSplash = 'greensplash'
+        } else if (this.props.color === 'red') {
+            colorSplash = 'redsplash'
+        }
+        
         return (
-            <div>This is Modal Cover Pic</div>
+            <form onSubmit={this.handleSubmit}>
+                <div className="covpic_modal_background" onClick={this.props.closeModal}></div>
+                <div className="covpic_modal_child">
+                    <div className="covpictop">
+                        <h1 className="covpictitle">Change Cover Picture!</h1>
+                        <img className="X" src={window.x_url} onClick={this.props.closeModal}/>
+                    </div>
+                    <div className="covpiclinediv"></div>
+                    <div className="covpicbody">
+                    </div>
+                    <div className="covpicsubmit">
+                        <input className={'submitbutton splashbutton ' + colorSplash} type="submit" value="Update Info"/>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
--- frontend/components/modal/modal_profpic.jsx
+++ frontend/components/modal/modal_profpic.jsx
@@ -8,8 +8,31 @@ class ModalProfPic extends React.Component {
     }
 
     render() {
+        let colorSplash;
+        if (this.props.color === 'blue') {
+            colorSplash = 'bluesplash';
+        } else if (this.props.color === 'green') {
+            colorSplash = 'greensplash'
+        } else if (this.props.color === 'red') {
+            colorSplash = 'redsplash'
+        }
+        
         return (
-            <div>This is Modal Profile Pic</div>
+            <form onSubmit={this.handleSubmit}>
+                <div className="profpic_modal_background" onClick={this.props.closeModal}></div>
+                <div className="profpic_modal_child">
+                    <div className="profpictop">
+                        <h1 className="profpictitle">Change Profile Picture!</h1>
+                        <img className="X" src={window.x_url} onClick={this.props.closeModal}/>
+                    </div>
+                    <div className="profpiclinediv"></div>
+                    <div className="profpicbody">
+                    </div>
+                    <div className="profpicsubmit">
+                        <input className={'submitbutton splashbutton ' + colorSplash} type="submit" value="Update Info"/>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
