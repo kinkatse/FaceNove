0e3a1649768e94c360e27915736172bb54dc568a
kakintse <kinkatse@mail.adelphi.edu>
2022-01-11 03:22:31 -0500

    Finish Post Controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,18 +1,55 @@
 class Api::PostsController < ApplicationController
 
     def create
-    end
-
-    def show
+        @post = Post.new(post_params)
+        if @post.save
+            puts "Post saved!"
+            # render "api/posts/show"
+            render :show
+        else
+            render json: @post.errors.full_messages, status: 422
+        end
     end
 
     def index
+        @posts = Post.all
+        render :index
+    end
+
+    def show
+        @post = Post.find_by(id: params[:id])
+        if @post
+            render :show
+        else
+            render json: ["Post does not exist"], status: 404
+        end
     end
 
     def update
+        @post = Post.find_by(id: params[:id])
+        if @post.update_attribute(post_params)
+            render :show
+        else
+            render json: @post.errors.full_messages, status: 418
+        end
     end
 
     def destroy
+        @post = Post.find_by(id: params[:id])
+        if @post
+            @post.destroy
+            render :show
+        else
+            render json: @post.errors.full_messages, status: 418
+        end
+    end
+
+    def post_params
+        params.require(:post).permit(
+            :post,
+            :user_id,
+            :postPhotoUrl
+        )
     end
 
 end
