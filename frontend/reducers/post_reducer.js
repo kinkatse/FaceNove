5ec7eed7a3c0411ae19a702f7c18b249ad93f570
kakintse <kinkatse@mail.adelphi.edu>
2022-01-11 04:11:52 -0500

    Finish post reducer

--- frontend/reducers/post_reducer.js
+++ frontend/reducers/post_reducer.js
@@ -4,9 +4,13 @@ const postsReducer = (postState = {}, action) => {
     Object.freeze(postState);
     switch(action.type) {
         case RECEIVE_ALL_POSTS:
-            return Object.assign({}, postState, action.posts)
+            return Object.assign({}, postState, action.posts);
         case RECEIVE_POST:
-            return Object.assign({}, postState, { [action.post.id]: action.post })
+            return Object.assign({}, postState, { [action.post.id]: action.post });
+        case REMOVE_POST:
+            let newState = Object.assign({}, postState);
+            delete newState[action.post.id];
+            return newState;
         default:
             return postState;
     }
