1f1e987a1c9d641fe4f3d6b775eee117a222227d
kakintse <kinkatse@mail.adelphi.edu>
2022-01-19 03:25:48 -0500

    Include bold formatting to profile intro

--- frontend/components/profile/profile_intro.jsx
+++ frontend/components/profile/profile_intro.jsx
@@ -15,9 +15,11 @@ class ProfileIntro extends React.Component {
         hometown, work, relationship, website, time;
         
         // Conditionals to make the info be default if no value
+        // Should make a button to make bio just like Facebook***
         if (user.bio) { bio = "Bio: " + user.bio }
         else { bio = "No Bio" }
 
+        // Should format birthday in English***
         if (user.birthdate) { birthday = user.birthdate.split('-') }
         else { birthday = "Birthday not registered" }
         if (user.birthdate !== "Birthday not registered") {
@@ -26,29 +28,35 @@ class ProfileIntro extends React.Component {
             birthdayFormatted = "Born on " + birthday.join('/')
         } else { "Something unexpected happened" }
 
-        if (user.gender) { gender = "Identify as " + user.gender }
-        else { gender = "Gender not registered" }
+        if (user.gender) {
+            gender = ["Identify as ", <strong key={user.gender}>{user.gender}</strong>]
+        } else { gender = "Gender not registered" }
 
-        if (user.hometown) { hometown = "Lives in " + user.hometown }
-        else { hometown = "Hometown not registered" }
+        if (user.hometown) {
+            hometown = ["Lives in ", <strong key={user.hometown}>{user.hometown}</strong>]
+        } else { hometown = "Hometown not registered" }
 
-        if (user.education) { education = "Studied at " + user.education }
-        else { education = "Education not registered" }
+        if (user.education) {
+            education = ["Studied at ", <strong key={user.education}>{user.education}</strong>]
+        } else { education = "Education not registered" }
 
-        if (user.work) { work = "Works as " + user.work }
-        else { work = "Work not registered" }
+        if (user.work) {
+            work = ["Works as ", <strong key={user.work}>{user.work}</strong>]
+        } else { work = "Work not registered" }
 
-        if (user.relationship) { relationship = "Currently " + user.relationship }
-        else { relationship = "Relationship not registered" }
+        if (user.relationship) {
+            relationship = ["Currently ", <strong key={user.relationship}>{user.relationship}</strong>]
+        } else { relationship = "Relationship not registered" }
 
-        if (user.website) { website = "Website: " + user.website }
-        else { website = "Website not registered" }
+        if (user.website) {
+            website = ["Website: ", <strong key={user.website}>{user.website}</strong>]
+        } else { website = "Website not registered" }
 
         return (
             <div className="profile_intro">
                 <h2 className="profbodytitle">Intro</h2>
                 <p className="profile_bio">{bio}</p>
-                <p className="profile_info">Email me at {user.email}</p>
+                <p className="profile_info">Email me at <strong>{user.email}</strong></p>
                 <p className="profile_info">{birthdayFormatted}</p>
                 <p className="profile_info">{gender}</p>
                 <p className="profile_info">{hometown}</p>
