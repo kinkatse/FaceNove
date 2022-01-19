f8407af8326772857daabc437ebaf55be8a3b80b
kakintse <kinkatse@mail.adelphi.edu>
2022-01-19 00:32:58 -0500

    Make a user with no posts show the create posts

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -25,24 +25,9 @@ class Post extends React.Component {
         }
     }
 
-    render() {
-        if (Object.keys(this.props.posts).length === 0) {
-            return null;
-        }
-
-        let postArr = Object.values(this.props.posts).reverse()
-
-        let profpicColor;
-        if (this.props.color === "blue") {
-            profpicColor = "postprofpicblue"
-        } else if (this.props.color === "green") {
-            profpicColor = "postprofpicgreen"
-        } else if (this.props.color === "red") {
-            profpicColor = "postprofpicred"
-        }
-
+    rendersCreatePost(profpicColor) {
         return (
-            <div className="">
+            <div>
                 <div className="profile_posts createpost">
                     <div className="createpost_profpic">
                         <Link to={`/user/${this.props.currentUser.id}`}>
@@ -66,6 +51,52 @@ class Post extends React.Component {
                 <div className="profile_posts">
                     <h2 className="profbodytitle">Posts</h2>
                 </div>
+            </div>
+        )
+    }
+
+    render() {
+        if (Object.keys(this.props.posts).length === 0) {
+            return this.rendersCreatePost()
+        }
+
+        let postArr = Object.values(this.props.posts).reverse()
+
+        let profpicColor;
+        if (this.props.color === "blue") {
+            profpicColor = "postprofpicblue"
+        } else if (this.props.color === "green") {
+            profpicColor = "postprofpicgreen"
+        } else if (this.props.color === "red") {
+            profpicColor = "postprofpicred"
+        }
+
+        return (
+            <div>
+                {/* <div className="profile_posts createpost">
+                    <div className="createpost_profpic">
+                        <Link to={`/user/${this.props.currentUser.id}`}>
+                            <img
+                                className={'createpost_profile_pic ' + profpicColor}
+                                src={this.props.currentUser.profilePicUrl}
+                            />
+                        </Link>
+                    </div>
+                    <div
+                        className="createpost_modalopen"
+                        onClick={this.props.openCreatePostModal}>
+                            What's on your mind?
+                    </div>
+                    <div className="createpostlinediv"></div>
+                    <div className="post_buttons">
+                        <h2 className="post_placeholder">Photo?</h2>
+                        <h2 className="post_placeholder">Other</h2>
+                    </div>
+                </div>
+                <div className="profile_posts">
+                    <h2 className="profbodytitle">Posts</h2>
+                </div> */}
+                {this.rendersCreatePost(profpicColor)}
                 {
                     postArr.map(post => (
                         <UserPost
