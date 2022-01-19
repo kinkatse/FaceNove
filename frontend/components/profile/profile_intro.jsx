62da3fe31248c6fae5640cb7a6e9c2af08e22cc3
kakintse <kinkatse@mail.adelphi.edu>
2022-01-19 10:53:49 -0500

    Formatted the birthday on profile intro better

--- frontend/components/profile/profile_intro.jsx
+++ frontend/components/profile/profile_intro.jsx
@@ -14,17 +14,75 @@ class ProfileIntro extends React.Component {
         hometown, work, relationship, website, time;
         
         // Conditionals to make the info be default if no value
-        // Should make a button to make bio just like Facebook***
+        // Should make a button to make bio just like Facebook, with better formatting too***
         if (user.bio) { bio = "Bio: " + user.bio }
         else { bio = <em>No Bio</em> }
+
+        const monthsObj = {
+            1: "January",
+            2: "February",
+            3: "March",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December"
+        }
+
+        const monthsFormat = (monthNum) => {
+            // debugger
+            // let monthsArr = [];
+            // for (let i = 1; i <= 12; i++) {
+            //     monthsArr.push(<option value={i} key={`month-${i}`}>{monthsObj[i]}</option>)
+            // }
+            // return monthsArr;
+            return monthsObj[parseInt(monthNum)]
+        }
+
+        const daysFormat = (dayNum) => {
+            // let daysArr = [];
+            // for (let i = 1; i <= 31; i++) {
+            //     daysArr.push(<option value={i} key={`day-${i}`}>{i}</option>)
+            // }
+            // return daysArr;
+            // debugger
+            let dayInt = parseInt(dayNum)
+            if (dayNum.slice(-1) === "1" && dayNum !== "11") {
+                return `${dayInt}st,`
+            } else if (dayNum.slice(-1) === "2" && dayNum !== "12") {
+                return `${dayInt}nd,`
+            } else if (dayNum.slice(-1) === "3" && dayNum !== "13") {
+                return `${dayInt}rd,`
+            // } else if (dayNum === "21") {
+            //     return "-21st,-"
+            // } else if (dayNum === "22") {
+            //     return "-22nd,-"
+            // } else if (dayNum === "23") {
+            //     return "-23rd,-"
+            // } else if (dayNum === "31") {
+            //     return "-31st,-"
+            } else {
+                return `${dayInt}th,`
+            }
+        }
 
         // Should format birthday in English***
         if (user.birthdate) { birthday = user.birthdate.split('-') }
         else { birthday = "Birthday not registered" }
         if (user.birthdate !== "Birthday not registered") {
-            let year = birthday.shift()
-            birthday.push(year)
-            birthdayFormatted = "Born on " + birthday.join('/')
+            // let year = birthday.shift()
+            // birthday.push(year)
+            debugger
+            let day = daysFormat(birthday.pop())
+            let month = monthsFormat(birthday.pop())
+            birthday.unshift(day)
+            birthday.unshift(month)
+            debugger
+            birthdayFormatted = "Born on " + birthday.join(' ')
         } else { "Something unexpected happened" }
 
         if (user.gender) {
