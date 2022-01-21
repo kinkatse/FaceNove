d253f64298c6e7a518de4ce9ad144cc7810d9976
kakintse <kinkatse@mail.adelphi.edu>
2022-01-20 21:49:21 -0500

    Fixed seconds, minutes, hours and days timestamp

--- frontend/components/posts/user_post.jsx
+++ frontend/components/posts/user_post.jsx
@@ -113,14 +113,12 @@ class UserPost extends React.Component {
         // Removing seconds
         time[0] = time[0].split(":").slice(0, 2).join(":")
 
-        // debugger
         // This is for the simple show of time posted
         if (type === "created_simple") {
             date.shift()
             return date.join(" ")
         }
 
-        debugger
         // Joining all of the date and time info into one string
         if (type === "edited_hover") {
             date.unshift("Edited on")
@@ -139,32 +137,29 @@ class UserPost extends React.Component {
 
         let datetime_created = new Date(this.props.created_at)
         let created = this.rendersTime(datetime_created, "created_hover")
-        debugger
         let datetime_updated = new Date(this.props.updated_at)
         let updated;
-        debugger
         // This is for when the post has never updated so we shouldn't
         // give updated any info if the post created and post updated
         // are the same. We need the conditional to have Date.parse since
         // comparing strings will always be false so we numbers work
         if (Date.parse(datetime_created) !== Date.parse(datetime_updated)) {
-            debugger
             updated = this.rendersTime(datetime_updated, "edited_hover")
         }
-        debugger
 
         // Formating time for when a post is under 1 day old
         let created_milli = Date.parse(datetime_created)
-        let now_milli = Date.parse(Date.now())
+        let now_milli = Date.now()
         // Grabbing time now and time posted and subtracting their time in
         // in milliseconds (with the Math.abs to make sure no negative)
         let time_diff = Math.abs(now_milli - created_milli)
         // Math.ceil is just to round number, we divide the time in milliseconds
         // by milliseconds per second, seconds per minute and minutes per hour
-        // to get time in hours. Did that for seconds and minutes too
+        // to get time in hours. Did that for seconds and minutes and days (6) too
         let time_diff_seconds = Math.ceil(time_diff / (1000))
         let time_diff_minutes = Math.ceil(time_diff / (1000 * 60))
         let time_diff_hours = Math.ceil(time_diff / (1000 * 60 * 60))
+        let time_diff_days = Math.ceil(time_diff / (1000 * 60 * 60 * 24))
         let time_posted;
         if (time_diff_seconds < 60) {
             time_posted = (time_diff_seconds.toString() + "s")
@@ -172,49 +167,11 @@ class UserPost extends React.Component {
             time_posted = (time_diff_minutes.toString() + "m")
         } else if (time_diff_hours < 24) {
             time_posted = (time_diff_hours.toString() + "h")
+        } else if (time_diff_days < 7) {
+            time_posted = (time_diff_days.toString() + "d")
         } else {
             time_posted = this.rendersTime(datetime_created, "created_simple")
         }
-
-        debugger
-
-        // // Making arrays of the individual date info to adjust them for formatting
-        // let date = time_created.toDateString().split(" ")
-        // let time = time_created.toLocaleTimeString().split(" ")
-
-        // // Making date of week full date with comma
-        // switch(date[0]) {
-        //     case 'Mon': date[0] = 'Monday,'; break
-        //     case 'Tue': date[0] = 'Tuesday,'; break
-        //     case 'Wed': date[0] = 'Wednesday,'; break
-        //     case 'Thu': date[0] = 'Thursday,'; break
-        //     case 'Fri': date[0] = 'Friday,'; break
-        //     case 'Sat': date[0] = 'Saturday,'; break
-        //     case 'Sun': date[0] = 'Sunday,'; break
-        //     default: date[0] = 'Day'; break
-        // }
-        // // Making month the full month
-        // switch(date[1]) {
-        //     case 'Jan': date[1] = 'January'; break
-        //     case 'Feb': date[1] = 'February'; break
-        //     case 'Mar': date[1] = 'March'; break
-        //     case 'Apr': date[1] = 'April'; break
-        //     case 'May': date[1] = 'May'; break
-        //     case 'Jun': date[1] = 'June'; break
-        //     case 'Jul': date[1] = 'July'; break
-        //     case 'Aug': date[1] = 'August'; break
-        //     case 'Sep': date[1] = 'September'; break
-        //     case 'Oct': date[1] = 'October'; break
-        //     case 'Nov': date[1] = 'November'; break
-        //     case 'Dec': date[1] = 'December'; break
-        //     default: date[1] = 'Month'; break
-        // }
-        // // Adding comma
-        // date[2] += ','
-        // // Removing seconds
-        // time[0] = time[0].split(":").slice(0, 2).join(":")
-        // // Joining all of the date and time info into one string
-        // let time_formatted = date.join(" ") + " at " + time.join(" ")
 
         return (
             <div className="post_whole">
@@ -232,7 +189,6 @@ class UserPost extends React.Component {
                                     {this.props.firstName} {this.props.lastName}
                                 </h2>
                             </Link>
-                            {/* <div className="post_timestamp">{this.props.updated_at}</div> */}
                             <div className="post_timestamp">
                                 {time_posted}
                             </div>
