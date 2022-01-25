de7cfda75ecce6bdac93e6acf2e98a0ee1f07609
kakintse <kinkatse@mail.adelphi.edu>
2022-01-25 00:06:22 -0500

    Attempting to edit photo in post

--- frontend/components/modal/modal_editpost.jsx
+++ frontend/components/modal/modal_editpost.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 class ModalEditPost extends React.Component {
     constructor(props) {
         super(props)
+        // debugger
         this.state = {
             post: this.props.postObj.post,
-            user_id: this.props.postObj.user_id
+            user_id: this.props.postObj.user_id,
+            photoUrl: this.props.postObj.postPhotoUrl || null,
+            photoFile: null
         }
         this.updatePostBody = this.updatePostBody.bind(this);
+        this.handleFile = this.handleFile.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -15,18 +19,110 @@ class ModalEditPost extends React.Component {
         return e => this.setState({ post: e.currentTarget.value })
     }
 
+    handleFile(e) {
+        const file = e.currentTarget.files[0];
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+            this.setState({photoFile: file, photoUrl: fileReader.result})
+        }
+        if (file) {
+            fileReader.readAsDataURL(file);
+        }
+    }
+
+    // handleSubmit(e) {
+    //     e.preventDefault();
+    //     const postData = Object.assign({}, this.state);
+    //     this.props.updatePost(postData, this.props.postObj.id)
+    //     .then(this.resetState())
+    // }
+
     handleSubmit(e) {
         e.preventDefault();
-        const postData = Object.assign({}, this.state);
-        this.props.updatePost(postData, this.props.postObj.id)
-        .then(this.resetState())
+        const postData = new FormData();
+        postData.append('postData[post]', this.state.post);
+        postData.append('postData[user_id]', this.state.user_id);
+        if (this.state.photoFile) {
+            postData.append('postData[postPhotoUrl]', this.state.photoFile);
+        }
+        if (this.state.post) {
+            this.props.createPost(postData)
+            .then(this.resetState())
+        } else {
+            // Need to have an error handler for when user doesn't
+            // give a post body. Otherwise we get 422 error from backend
+            alert("Post body needs a message")
+        }
+    }
+
+    resetPreview() {
+        this.setState({
+            photoUrl: null,
+            photoFile: null
+        })
     }
 
     resetState() {
         this.setState({
-            post: this.props.postObj.post
+            post: this.props.postObj.post,
+            photoUrl: null,
+            photoFile: null
         })
         this.props.closeModal()
+    }
+
+    rendersPreview() {
+        let preview;
+        if (this.state.photoUrl) {
+            preview =
+                ( 
+                <div className="postpic_whole">
+                    <img
+                        className="postpic_modal"
+                        src={this.state.photoUrl}
+                    />
+                    <div className="photo_close">
+                        <img
+                            className="photo_X"
+                            src={window.x_url}
+                            onClick={() => this.resetPreview()}
+                        />
+                    </div>
+                </div>
+                )
+        } else {
+            preview = ( <div className="postpic_noimage"> No Image Selected </div> )
+        }
+
+        return preview
+    }
+
+    rendersPictureButton(colorSplash) {
+        let photobutton;
+        if (!this.state.photoUrl) {
+            photobutton = (
+                <label className={'profpic_filebutton splashbutton profchoose ' + colorSplash}>
+                    <input type="file" onChange={this.handleFile}/>
+                    Add a Picture?
+                </label>
+            )
+        } else {
+            photobutton = (
+                <label className={'profpic_filebutton splashbutton profchoose ' + colorSplash}>
+                    <input type="file" onChange={this.handleFile}/>
+                    Change Picture?
+                </label>
+            )
+        }
+
+        return (
+            <div>
+                <div className="profpic_components">
+                    {this.rendersPreview()}
+                    {photobutton}
+                </div>
+            </div>
+        )
     }
 
     render() {
@@ -74,9 +170,7 @@ class ModalEditPost extends React.Component {
                             ></textarea>
                         </div>
                     </div>
-                    <div>
-                        {/* <p className="testEdit">Picture Edit Here</p> */}
-                    </div>
+                    {this.rendersPictureButton(colorSplash)}
                     <div className="editsubmit">
                         <input className={'editsubmitbutton splashbutton editpostposition ' + colorSplash} type="submit" value="Save"/>
                     </div>
