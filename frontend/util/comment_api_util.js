1778d01fa0433eb9fd179fc6f20ff5fc4085b40e
kakintse <kinkatse@mail.adelphi.edu>
2022-01-25 22:40:01 -0500

    Finish comments backend for the most part and also comment util

---
+++ frontend/util/comment_api_util.js
@@ -0,0 +1,40 @@
+export const getAllComments = () => {
+    return $.ajax({
+        url: '/api/comments',
+        method: 'GET'
+    })
+}
+
+// export const getComment = (commentId) => {
+//     return $.ajax({
+//         url: `/api/comments/${commentId}`,
+//         method: 'GET'
+//     })
+// }
+
+export const createComment = (commentData) => {
+    return $.ajax({
+        url: '/api/comments',
+        method: 'POST',
+        data: commentData,
+        contentType: false,
+        processData: false
+    })
+}
+
+export const editComment = (commentData, commentId) => {
+    return $.ajax({
+        url: `/api/comments/${commentId}`,
+        method: 'PATCH',
+        data: commentData,
+        contentType: false,
+        processData: false
+    })
+}
+
+export const deleteComment = () => {
+    return $.ajax({
+        url: `/api/comments/${commentId}`,
+        method: 'DELETE'
+    })
+}
