fe763866ca2b3316b74e32b9e9b162d020e3bac8
kakintse <kinkatse@mail.adelphi.edu>
2022-01-26 00:28:32 -0500

    Forgot to combine the comments reducer to the entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import users from './users_reducer';
 import posts from './posts_reducer';
+import comments from './comments_reducer';
 
 const entitiesReducer = combineReducers({
     users,
-    posts
+    posts,
+    comments
 })
 
 export default entitiesReducer;
