5dfb1ca77832d7c73188192178f7b0255fb522f9
kakintse <kinkatse@mail.adelphi.edu>
2022-02-01 23:52:03 -0500

    Making like table through migrations

--- db/migrate/20220201045521_create_likes.rb
+++ db/migrate/20220201045521_create_likes.rb
@@ -1,6 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
+      t.boolean :liked, null: false
+      t.integer :liker_id, null: false
+      t.integer :post_id, null: false
     end
+    add_index :likes, :liker_id
+    add_index :likes, :post_id
   end
 end
