57e13fadcdc6fe743b5d3fabd1a4de267443c001
kakintse <kinkatse@mail.adelphi.edu>
2022-07-09 15:37:05 -0400

    Add like count in the modal

--- frontend/components/modal/modal_likes.jsx
+++ frontend/components/modal/modal_likes.jsx
@@ -16,19 +16,6 @@ class LikesModal extends React.Component {
         post.likeIds.map((likeId) => {
             likes.push(this.props.likesState[likeId])
         })
-        // debugger
-        // let likers = likes.forEach((like) => {
-        //     <div className='likes_modal'>
-        //         <Link to={`/user/${like.liker_id}`}>
-        //             <img
-        //                 className={`post_profile_pic ${postProfPicColor()}`}
-        //                 src={like.profilePicUrl}
-        //             />
-        //         </Link>
-        //         <Link to={`/user/${like.liker_id}`}>{like.firstName} {like.lastName}</Link>
-        //     </div>
-        // })
-        // return likers
 
         return (<div>
             {likes.map((like) => {
@@ -53,11 +40,11 @@ class LikesModal extends React.Component {
                 <div className="profpic_modal_background" onClick={this.props.closeModal}></div>
                 <div className="profpic_modal_child">
                     <div className="profpictop">
-                        <h1 className="profpictitle">Likes</h1>
+                        <h1 className="like_modal_title">Likes</h1>
+                        <h2 className="like_amount">{this.props.postObj.likeIds.length}</h2>
                         <img className="X" src={window.x_url} onClick={this.props.closeModal}/>
                     </div>
                     <div className="profpiclinediv"></div>
-                    <h1 onClick={this.props.closeModal}>H1 from likes Modal</h1>
                     {this.rendersLikers()}
                 </div>
             </>
