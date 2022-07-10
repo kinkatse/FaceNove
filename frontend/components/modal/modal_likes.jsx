d7e8de8b34f12e26a69294fa614d9b2e95460bf4
kakintse <kinkatse@mail.adelphi.edu>
2022-07-10 05:20:55 -0400

    Style Like count and make modal not so big, also scrollable

--- frontend/components/modal/modal_likes.jsx
+++ frontend/components/modal/modal_likes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { appColor, postProfPicColor } from '../../util/color_util';
+import { appColor, postProfPicColor, unlikeButton } from '../../util/color_util';
 
 class LikesModal extends React.Component {
     constructor(props) {
@@ -31,6 +31,10 @@ class LikesModal extends React.Component {
                             {like.firstName} {like.lastName}
                         </span>
                     </Link>
+                    <img
+                        className="post_like_buttons post_buttons_bigger"
+                        src={unlikeButton()}
+                    />
                 </div>)
             })}
         </div>)
@@ -40,10 +44,16 @@ class LikesModal extends React.Component {
         return (
             <>
                 <div className="profpic_modal_background" onClick={this.props.closeModal}></div>
-                <div className="profpic_modal_child">
+                <div className="like_modal_child">
                     <div className="profpictop">
                         <h1 className="like_modal_title">Likes</h1>
                         <h2 className="like_amount">{this.props.postObj.likeIds.length}</h2>
+                        <span className='like_modal_title_visual'>
+                            <img
+                                className="post_like_buttons post_buttons_bigger"
+                                src={unlikeButton()}
+                            />
+                        </span>
                         <img className="X" src={window.x_url} onClick={this.props.closeModal}/>
                     </div>
                     <div className="profpiclinediv"></div>
