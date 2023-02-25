ceaa9c5f1460f9f12205a26fc0fc9076786669ce
kakintse <kinkatse@mail.adelphi.edu>
2023-02-25 00:25:31 -0500

    Attempt mini search bar

--- frontend/components/navbar/navbar_left.jsx
+++ frontend/components/navbar/navbar_left.jsx
@@ -4,6 +4,30 @@ import { Link } from 'react-router-dom';
 import { logoImage } from '../../util/color_util';
 
 const NavBarLeft = ({ currentUser }) => {
+    let miniSearchOn = false
+    let searchInput = null
+    let searchBarEl = document.querySelector(".search-bar")
+    let leftBarEl = document.querySelector(".navbar_left")
+    debugger
+    if (searchBarEl && leftBarEl &&
+        (leftBarEl.offsetWidth - 20 < searchBarEl.offsetWidth )) {
+            miniSearchOn = true
+    }
+
+    if (miniSearchOn) {
+        searchInput = (
+            <div className='search'>
+                <input className='search-bar-mini' type="text" placeholder='O' />
+            </div>
+        )
+    } else {
+        searchInput = (
+            <div className='search'>
+                <input className='search-bar' type="text" placeholder='Search FaceNove' />
+            </div>
+        )
+    }
+
     return (
         <div className="navbar_left">
             <div className="logo">
@@ -11,9 +35,12 @@ const NavBarLeft = ({ currentUser }) => {
                     <img src={logoImage()} />
                 </Link>
             </div>
-            <div className='search'>
-                <input className='search-bar' type="text" placeholder='Search FaceNove' />
-            </div>
+            {searchInput}
+            {/* { miniSearchOn && (
+                <div className='search'>
+                    <input className='search-bar' type="text" placeholder='Search FaceNove' />
+                </div>
+            )} */}
             {/* <div className={`navprofpic ${profPicColor()}`}>
                 <Link to={`/user/${currentUser.id}`}>
                     <img src={currentUser.profilePicUrl}/>
