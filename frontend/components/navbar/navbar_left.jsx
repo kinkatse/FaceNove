c058d92ade2e0ac5ab31ddb14e75199fb350b42f
kakintse <kinkatse@mail.adelphi.edu>
2023-02-25 01:11:06 -0500

    First attempt to make search bar a little more dynamic

--- frontend/components/navbar/navbar_left.jsx
+++ frontend/components/navbar/navbar_left.jsx
@@ -3,51 +3,69 @@ import { Link } from 'react-router-dom';
 
 import { logoImage } from '../../util/color_util';
 
-const NavBarLeft = ({ currentUser }) => {
-    let miniSearchOn = false
-    let searchInput = null
-    let searchBarEl = document.querySelector(".search-bar")
-    let leftBarEl = document.querySelector(".navbar_left")
-    debugger
-    if (searchBarEl && leftBarEl &&
-        (leftBarEl.offsetWidth - 20 < searchBarEl.offsetWidth )) {
-            miniSearchOn = true
+class NavBarLeft extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchBarOpen: false
+        }
+        this.toggleSearch = this.toggleSearch.bind(this)
     }
 
-    if (miniSearchOn) {
-        searchInput = (
-            <div className='search'>
-                <input className='search-bar-mini' type="text" placeholder='O' />
-            </div>
-        )
-    } else {
-        searchInput = (
-            <div className='search'>
-                <input className='search-bar' type="text" placeholder='Search FaceNove' />
-            </div>
-        )
+    toggleSearch() {
+        this.setState({searchBarOpen: !this.state.searchBarOpen})
     }
 
-    return (
-        <div className="navbar_left">
-            <div className="logo">
-                <Link to="/">
-                    <img src={logoImage()} />
-                </Link>
-            </div>
-            {searchInput}
-            {/* { miniSearchOn && (
+    searchInput() {
+        let miniSearchOn = false
+        let searchInput = null
+        let searchBarEl = document.querySelector(".search-bar")
+        let leftBarEl = document.querySelector(".navbar_left")
+        
+        if (searchBarEl && leftBarEl &&
+            (leftBarEl.offsetWidth - 20 < searchBarEl.offsetWidth
+            && miniSearchOn !== true)) {
+                miniSearchOn = true
+        }
+    
+        if (miniSearchOn) {
+            searchInput = (
+                <div className='search' onClick={this.toggleSearch}>
+                    <input className='search-bar-mini' type="text" placeholder='O' disabled/>
+                </div>
+            )
+        } else if (this.state.searchBarOpen) {
+            searchInput = (
+                <div className='search'>
+                    <span className="magnifying-glass" onClick={this.toggleSearch}>O</span>
+                    <input className='search-bar-magni' type="text" placeholder='Search FaceNove' />
+                </div>
+            )
+        } else {
+            searchInput = (
                 <div className='search'>
                     <input className='search-bar' type="text" placeholder='Search FaceNove' />
                 </div>
-            )} */}
-            {/* <div className={`navprofpic ${profPicColor()}`}>
-                <Link to={`/user/${currentUser.id}`}>
-                    <img src={currentUser.profilePicUrl}/>
-                </Link>
-            </div> */}
-        </div>
-    )
+            )
+        }
+
+        return searchInput
+    }
+
+    render() {
+    
+        return (
+            <div className="navbar_left">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={logoImage()} />
+                    </Link>
+                </div>
+                {this.searchInput()}
+            </div>
+        )
+    }
 }
 
 export default NavBarLeft;
