e02f4041c1325d7ce5317895dab5dbfbce059f02
kakintse <kinkatse@mail.adelphi.edu>
2023-02-27 11:33:42 -0500

    add post creations in the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,48 @@ kinka.coverPicUrl.attach(io: File.open("app/assets/images/User_Kin_CovPic.png"),
 bob.coverPicUrl.attach(io: File.open("app/assets/images/User_Bob_CovPic.png"), filename: "User_Bob_CovPic.png")
 carly.coverPicUrl.attach(io: File.open("app/assets/images/User_Carly_CovPic.png"), filename: "User_Carly_CovPic.png")
 
+# slightly randomized order for a better looking index:
 Post.create!(body: "Hello, this is Demo's first post", user_id: demo.id)
-Post.create!(body: "Oh my god, this is my second post!", user_id: demo.id)
 Post.create!(body: "I like to play tennis, does anyone want to play?", user_id: kinka.id)
+Post.create!(body: "I'm a barbie girl and a carly girl", user_id: carly.id)
+Post.create!(body: "Skiing is cool", user_id: bob.id)
+Post.create!(body: "Oh my god, this is my second post!", user_id: demo.id)
+Post.create!(body: "I am really liking the color theme function for this app because I can change it to my favorite color, green!", user_id: kinka.id)
+Post.create!(body: "I love mochi <3", user_id: carly.id)
+Post.create!(body: "Does anyone wanna talk about that last Demon Slayer episode?! That animation was insane!! One day, I want to be able to do something creative to that extent", user_id: kinka.id)
+Post.create!(body: "I just fell into the soft snow. It was nice.", user_id: bob.id)
+Post.create!(body: "I'm getting sick of snow in Canada these days. I just wanna go out and have fun! A trip under the hot sun would be nice...", user_id: carly.id)
+Post.create!(body: "I can create, read, update and delete posts!", user_id: demo.id)
+Post.create!(body: "Sometimes when no one is looking, I put some snow in my mouth and imagine I'm eating ice cream", user_id: bob.id)
+Post.create!(body: "The food in NYC is great but the food in Hong Kong is unmatched. I miss the food over there!", user_id: kinka.id)
+Post.create!(body: "I like dancing but wish I could do it as a living. It's hard to make money in that industry unless you are really good.", user_id: carly.id)
+Post.create!(body: "What? People think I'm too obsessed with skiing and want me to talk about something else. Well I like to water ski as well", user_id: bob.id)
+Post.create!(body: "I have recently been getting into cooking and use HelloFresh. It's been a fun experience so far and learned a lot with how ingredients mix. I think my prep skills have improved since the first day it would take me around 30 minutes to prep the ingredients but now I can do it in half the time! But the mess in the kitchen after I cook is the worst part. I wish I could just avoid that because that can take an additional hour every time and feels like a major waste of time... But the food is good and somewhat affordable so that's a good balance, especially since it's cheaper and healthier than ordering from UberEats.", user_id: kinka.id)
+Post.create!(body: "Can someone give me a ride to the mall...?", user_id: carly.id)
+Post.create!(body: "Welcome to FaceNove!", user_id: demo.id)
+
+# # demo posts
+# Post.create!(body: "Hello, this is Demo's first post", user_id: demo.id)
+# Post.create!(body: "Oh my god, this is my second post!", user_id: demo.id)
+# Post.create!(body: "I can create, read, update and delete posts!", user_id: demo.id)
+# Post.create!(body: "Welcome to FaceNove!", user_id: demo.id)
+
+# # carly posts
+# Post.create!(body: "I'm a barbie girl and a carly girl", user_id: carly.id)
+# Post.create!(body: "I love mochi <3", user_id: carly.id)
+# Post.create!(body: "I'm getting sick of snow in Canada these days. I just wanna go out and have fun! A trip under the hot sun would be nice...", user_id: carly.id)
+# Post.create!(body: "I like dancing but wish I could do it as a living. It's hard to make money in that industry unless you are really good.", user_id: carly.id)
+# Post.create!(body: "Can someone give me a ride to the mall...?", user_id: carly.id)
+
+# # kinka posts
+# Post.create!(body: "I like to play tennis, does anyone want to play?", user_id: kinka.id)
+# Post.create!(body: "I am really liking the color theme function for this app because I can change it to my favorite color, green!", user_id: kinka.id)
+# Post.create!(body: "Does anyone wanna talk about that last Demon Slayer episode?! That animation was insane!! One day, I want to be able to do something creative to that extent", user_id: kinka.id)
+# Post.create!(body: "The food in NYC is great but the food in Hong Kong is unmatched. I miss the food over there!", user_id: kinka.id)
+# Post.create!(body: "I have recently been getting into cooking and use HelloFresh. It's been a fun experience so far and learned a lot with how ingredients mix. I think my prep skills have improved since the first day it would take me around 30 minutes to prep the ingredients but now I can do it in half the time! But the mess in the kitchen after I cook is the worst part. I wish I could just avoid that because that can take an additional hour every time and feels like a major waste of time... But the food is good and somewhat affordable so that's a good balance, especially since it's cheaper and healthier than ordering from UberEats.", user_id: kinka.id)
+
+# # bob posts
+# Post.create!(body: "Skiing is cool", user_id: bob.id)
+# Post.create!(body: "I just fell into the soft snow. It was nice.", user_id: bob.id)
+# Post.create!(body: "Sometimes when no one is looking, I put some snow in my mouth and imagine I'm eating ice cream", user_id: bob.id)
+# Post.create!(body: "What? People think I'm too obsessed with skiing and want me to talk about something else. Well I like to water ski as well", user_id: bob.id)
