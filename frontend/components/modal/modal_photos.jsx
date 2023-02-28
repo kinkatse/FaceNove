c7e727142cebee1ddd4ee1fe91e156caa98cd779
kakintse <kinkatse@mail.adelphi.edu>
2023-02-28 01:18:09 -0500

    get more information onto the photo modal, just need likes and some more post information and then styling

--- frontend/components/modal/modal_photos.jsx
+++ frontend/components/modal/modal_photos.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import { appColor, postProfPicColor, unlikeButton } from '../../util/color_util';
+import CommentContainer from '../comments/comment_container';
 
 class PhotoModal extends React.Component {
     constructor(props) {
@@ -40,14 +41,41 @@ class PhotoModal extends React.Component {
     //     </div>)
     // }
 
+    rendersPostTopRight() {
+        let component;
+        // let dropclassname;
+        // if (this.state.dropOpen) {
+        //     dropclassname = ""
+        // } else {
+        //     dropclassname = "opendropbtn"
+        // }
+
+        // if (this.props.currentUser.id === parseInt(this.props.postUserId)) {
+        //     component = (
+        //         <div>
+        //             {this.rendersPostDropClose()}
+        //             <div className={`post_top_right ${dropclassname}`} onClick={this.dropOpen}>
+        //                 {this.rendersPostDrop()}
+        //             </div>
+        //         </div>
+        //     )
+        // } else {
+        //     component = null;
+        // }
+        return (
+            component
+        )
+    }
+
     render() {
 
-        debugger
+        // debugger
         return (
             <>
                 <div className="profpic_modal_background" onClick={this.props.closeModal}></div>
                 <div className="like_modal_child">
                     <div className="profpictop">
+                        {/* Photo */}
                         <h1 className="like_modal_title">Photos</h1>
                         <img
                             key={this.props.postObj.id}
@@ -57,6 +85,42 @@ class PhotoModal extends React.Component {
                         <img className="X" src={window.x_url} onClick={this.props.closeModal}/>
                     </div>
                     <div className="profpiclinediv"></div>
+                    <div className="postlinediv"></div>
+                    {/* Post */}
+                    <div className="post_top">
+                        <div className="post_top_left">
+                            <Link to={`/user/${this.props.postObj.user_id}`}>
+                                <img
+                                    className={`post_profile_pic ${postProfPicColor()}`}
+                                    src={this.props.postObj.profilePicUrl}
+                                />
+                            </Link>
+                            <div className="name_and_time">
+                                <Link to={`/user/${this.props.postObj.user_id}`}>
+                                    <h2 className="post_name">
+                                        {this.props.postObj.firstName} {this.props.postObj.lastName}
+                                    </h2>
+                                </Link>
+                                {/* <div className="post_timestamp">
+                                    {time_posted}
+                                    <div className="post_timestamp_hover">
+                                        {timestamp_hover}
+                                    </div>
+                                </div> */}
+                            </div>
+                        </div>
+                        {this.rendersPostTopRight()}
+                    </div>
+                    <div className="post_middle">
+                        <p className="post_body">
+                            {this.props.postObj.body}
+                        </p>
+                    </div>
+                    {/* Comments */}
+                    <CommentContainer
+                        postUserId={this.props.postObj.user_id}
+                        postId={this.props.postObj.id}
+                    />
                 </div>
             </>
         )
